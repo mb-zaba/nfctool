8dee43ced80be570d6e8dcc77203053b6a69c41f
zaba <mat44bon@gmail.com>
2020-06-12 20:22:51 +0200

    Added new version

--- outil-nfc.py
+++ outil-nfc.py
@@ -1,13 +1,18 @@
 """
 --- Script de récupération des données des capteurs via le NFC Reader
 
+<<<<<<< Updated upstream
+=======
+version: 0.2.1
+
+>>>>>>> Stashed changes
 --- Librairies utilisée:
 pyscard : Fait la liaison avec le lecteur NFC
 nfcpy : Permet de récupérer les informations des capteurs
 xlwt : Permet d'écrire un fichier Excel
 """
 
-import sys, nfc, xlwt
+import sys, nfc, xlwt, os
 
 # tant que le programme continue, c'est qu'il y a une carte sur le lecteur lorsque l'on tape Entrée
 try:
@@ -15,6 +20,7 @@ try:
 	sheet = workbook.add_sheet('Sheet_1')
 	row_num = 0
 	autre = 'o'
+<<<<<<< Updated upstream
 	# La boucle permet de rentrer plusieurs capteurs les uns après les autres
 	while autre == 'o':
 		# connexion à la carte NFC par le lecteur USB
@@ -56,3 +62,108 @@ try:
 
 except:
 	print("\nArrêt du programme.")
+=======
+	clés = []
+	try:
+		# La boucle permet de rentrer plusieurs capteurs les uns après les autres
+		while autre == 'o':
+			# contient les données de bases attendues
+			donnees = {
+				'DevEui':'',
+				'Ota':'',
+				'Ack':'',
+				'AppEui':'',
+				'AppKey':'',
+				'SplPer':'',
+				'TempPer':'',
+				'LightPer':'',
+				'PirPer':'',
+				'PirCfg':'',
+				'PirSens':'',
+				'EyePer':'',
+				'SendPer':'',
+				'VddPer':'',
+				'PerOvr':'',
+				'DrDef':'',
+				'DrMax':'',
+				'DrMin':'',
+				'Plan':'',
+				'Link':'',
+				'QSize':'',
+				'QOffset':'',
+				'QPurge':'',
+				'Port':'',
+				'Plans':'',
+				'Sensor':'',
+				'FW':''
+			}
+			try:
+				# connexion à la carte NFC par le lecteur USB
+				clf = nfc.ContactlessFrontend('usb')
+
+				# récupération des données du capteur et mise en forme en dictionnaire
+				tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+				assert tag.ndef is not None
+				records = str(tag.ndef.records[0]).split('\'')
+				data = records[3].split('\n')
+				for i in range(0, len(data)-1):
+					info = data[i].split(':')
+					donnees[info[0]] = info[1]
+				print('Capteur lu')
+				# démarrage de l'écriture dans un fichier Excel
+				# écriture des titres des colonnes
+				if row_num == 0:
+					row = sheet.row(row_num)
+					col_num = 1
+					for clé in donnees:
+						row.write(col_num, clé)
+						clés.append(clé)
+						col_num += 1
+					row_num += 1
+
+				for clé in donnees:
+					if clé not in clés:
+						sheet.write(0, col_num, clé)
+						col_num += 1
+	
+
+				# écriture des données
+				row = sheet.row(row_num)
+				row.write(0, f'Capteur {row_num}')
+				col_num = 1
+				for valeur in donnees.values():
+					row.write(col_num, valeur)
+					col_num += 1
+				row_num += 1
+
+			except:
+				print(f'Pas de capteur\n{sys.exc_info()[0]}')
+
+			finally:
+				clf.close()
+				autre = str(input('Un autre capteur? (o/n)'))
+
+	finally:
+		nom_fichier = str(input('Nom du fichier Excel: '))
+		workbook.save(f"donnees sorties\\{nom_fichier}.xls")
+
+def ecriture():
+	num_line = 1
+	filename = str(input('Nom complet du fichier: '))
+	file = open(filename, 'r')
+	for line in file:
+		if num_line == 1:
+			line.split(';')
+			print(line)
+
+
+print('Lecteur NFC.\nTapez "l" pour lire les données du capteur ou "e" pour écrire les données dans le capteur.')
+print('Le mode écriture lit les données d\'un fichier CSV, placez le dans le même dossier que l\'application.')
+mode = str(input('> '))
+while mode not in ('l', 'e'):
+	mode = str(input('> '))
+if mode == 'e':
+	ecriture()
+else:
+	lecture()
+>>>>>>> Stashed changes
