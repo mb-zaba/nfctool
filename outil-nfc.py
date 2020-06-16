508665c56273215012dc4bfd8c77d2c6bf14036f
zaba <mat44bon@gmail.com>
2020-06-16 15:25:48 +0200

    Update outil-nfc.py
    - ajouté le parser
    - création de deux fichiers différents pour les données et l'injection

--- outil-nfc.py
+++ outil-nfc.py
@@ -1,26 +1,51 @@
 """
 --- Script de récupération des données des capteurs via le NFC Reader
 
-version: 0.3
+version: 0.4.0
 
 --- Librairies utilisée:
 nfcpy : Permet de récupérer les informations des capteurs
 xlsxwriter : Permet d'écrire des fichier xlsx pour Excel
+
+Create/update
+pas les données
 """
 
-import sys, nfc, os, xlsxwriter
+import sys, nfc, os, xlsxwriter, argparse
 
-# affiche l'aide
-def aide():
-	print('utilisation:\tpython3 lecteur_nfc.py mode nom_fichier\n')
-	print('mode:')
-	print('\t--lecture nom_fichier\t\tLit les données des capteurs et sort un fichier Excel')
-	print('\t--ecriture nom_fichier\t\tLit les données d\'un fichier CSV donné en argument\n\t\t\t\t\tet écrit les données dans un capteur')
-	print('\t--help,-h\t\t\tAffiche cette aide')
-	print('\nProgramme de lecture et d\'écriture pour capteur ERS Eye.')
+# une fonction pour détecter les arguments et les utiliser dans le programme
+def arg_parser():
+	parser = argparse.ArgumentParser(description="Outil de lecture et d'écriture de capteur ERS Eye")
+
+	mode = parser.add_mutually_exclusive_group(required=True)
+	action = parser.add_mutually_exclusive_group()
+
+	# pour le mode lecture
+	mode.add_argument("-l", "--lire", help="mode lecture", action="store_true", dest="lecture")
+
+	# action lors de l'injection dans spot objenious
+	action.add_argument("-c", "--create",
+		help="création du capteur lors de l'injection dans spot objenious",
+		action="store_const", const="create")
+	action.add_argument("-u", "--update",
+		help="mise à jour du capteur lors de l'injection dans spot objenious",
+		action="store_const", const="update")
+
+	# pour le mode ecriture
+	mode.add_argument("-e", "--ecrire", help="mode ecriture", action="store_true", dest="ecriture")
+
+	# définir le nom du fichier en sortie de la lecture
+	parser.add_argument("-s", "--sortie",
+		help="défini le nom du fichier en sortie, suffixes -spot et -donnees seront ajouté",
+		metavar='nom_fichier')
+
+	# affiche toutes les données du capteur récupérées par le programme
+	parser.add_argument("-v", "--verbose", help="affiche les données du capteur", action="store_true")
+
+	return(parser.parse_args())
 
 # fonction de lecture du capteur
-def lecture(nom_fichier):
+def lecture(nom_fichier, action, verbose):
 	autre = 'o'
 	num_capt = 0
 	donnees = {}
@@ -35,6 +60,8 @@ def lecture(nom_fichier):
 		tag = clf.connect(rdwr={'on-connect': lambda tag: False})
 		print(f'Capteur lu')
 		records = tag.ndef.records[0]
+		if verbose:
+			print(records)
 		data = records.text.split('\n')
 
 		# rangement des données dans un dictionnaire
@@ -55,7 +82,6 @@ def lecture(nom_fichier):
 					donnees.get(info[0]).append(info[1])
 			else:
 				donnees.get(info[0]).append(info[1])
-		print(donnees)
 
 		clf.close()
 		num_capt += 1
@@ -64,11 +90,14 @@ def lecture(nom_fichier):
 
 	# écriture dans le fichier Excel
 	# Ouverture du fichier Excel et de la feuille
-	workbook = xlsxwriter.Workbook(f'donnees_sorties\\{nom_fichier}.xlsx')
-	worksheet = workbook.add_worksheet()
+	fichier_spot = xlsxwriter.Workbook(f'donnees_sorties\\{nom_fichier}-spot.xlsx')
+	fichier_data = xlsxwriter.Workbook(f'donnees_sorties\\{nom_fichier}-donnees.xlsx')
+	
+	spot_sheet = fichier_spot.add_worksheet()
+	data_sheet = fichier_data.add_worksheet()
 
 	# écriture en gras des titres de colonnes
-	bold = workbook.add_format({'bold': True})
+	bold = fichier_spot.add_format({'bold': True})
 
 	# titres de colonnes du template à respecter pour spot.objenious
 	template = ['Action (create / update) *',
@@ -89,31 +118,43 @@ def lecture(nom_fichier):
 	# la variable y est pour la ligne dans le tableau Excel
 	x = 0
 	for x in range(0, len(template)):
-		worksheet.write(f'{chr(x+65)}1', template[x], bold)
+		spot_sheet.write(f'{chr(x+65)}1', template[x], bold)
 
 		if 'DevEUI' in template[x]:
 			deveuis = donnees.get("DevEui")
 			y = 2
 			for deveui in deveuis:
-				worksheet.write(f'{chr(x+65)}{y}', deveui)
+				spot_sheet.write(f'{chr(x+65)}{y}', deveui)
 				y += 1
 		
 		elif 'AppEUI' in template[x]:
 			noms = donnees.get("AppEui")
 			y = 2
 			for nom in noms:
-				worksheet.write(f'{chr(x+65)}{y}', nom)
+				spot_sheet.write(f'{chr(x+65)}{y}', nom)
 				y += 1
 
 		elif 'AppKey' in template[x]:
 			appkeys = donnees.get("AppKey")
 			y = 2
 			for appkey in appkeys:
-				worksheet.write(f'{chr(x+65)}{y}', appkey)
+				spot_sheet.write(f'{chr(x+65)}{y}', appkey)
 				y += 1
 
-	# On commence à écrire le reste des données dans le fichier Excel, en évitant les données déjà écrites
-	x = 11
+		elif 'Profil' in template[x]:
+			for i in range(2, num_capt+2):
+				spot_sheet.write(f'{chr(x+65)}{i}', 'elsys-ers-cde4320234203')
+
+		elif 'Groupe' in template[x]:
+			for i in range(2, num_capt+2):
+				spot_sheet.write(f'{chr(x+65)}{i}', 'test_perverie')
+
+		elif 'Action' in template[x]:
+			for i in range(2, num_capt+2):
+				spot_sheet.write(f'{chr(x+65)}{i}', action)
+
+	# écriture du fichier de données
+	x = 0
 	alpha = True
 	for field in donnees:
 		# Cette partie vérifie que le code ascii des colonnes (de 65 à 90, soit de A à Z)
@@ -124,21 +165,20 @@ def lecture(nom_fichier):
 		else:
 			cell = f'A{chr(x+65)}'
 
-		if field not in ['DevEui', 'AppEui', 'AppKey']:
-			y = 2
-			for data in donnees[field]:
-				worksheet.write(f'{cell}{y}', data)
-				y += 1
-			worksheet.write(f'{cell}1', field, bold)
-			x += 1
+		y = 2
+		for data in donnees[field]:
+			data_sheet.write(f'{cell}{y}', data)
+			y += 1
+		data_sheet.write(f'{cell}1', field)
+		x += 1
 		if x > 25:
 			alpha = False
 			x = 0
-			
-
 	# On ferme le fichier Excel
-	workbook.close()
-	print(f"Fichier sauvegardé: donnees_sorties\\{nom_fichier}.xlsx")
+	fichier_spot.close()
+	fichier_data.close()
+	print(f"Fichier d'injection: donnees_sorties\\{nom_fichier}-spot.xlsx")
+	print(f"Fichier de données: donnees_sorties\\{nom_fichier}-donnees.xlsx")
 
 # fonction d'écriture du capteur
 def ecriture(filename):
@@ -156,13 +196,11 @@ def ecriture(filename):
 
 # début du programme
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		aide()
-	else:
-		if sys.argv[1] not in ('--ecriture', '--lecture'):
-			aide()
-		else:
-			if sys.argv[1] == '--ecriture':
-				ecriture(sys.argv[2])
-			elif sys.argv[1] == '--lecture':
-				lecture(sys.argv[2])
+	args = arg_parser()
+	try:
+		action = args.create
+	except:
+		action = args.update
+	finally:
+		if args.lecture:
+			lecture(args.sortie, action, args.verbose)
