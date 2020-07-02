3c5fee2eec982737204354a92df79eee2523ff98
zaba <mat44bon@gmail.com>
2020-07-02 12:01:12 +0200

    Update start-env.bat

--- start-env.bat
+++ start-env.bat
@@ -1,1 +1,4 @@
+@echo off
+IF exist donnees_sorties ( echo Ok ) ELSE ( mkdir donnees_sorties && echo Dossier donnees_sorties créé. )
+IF exist entrees_csv ( echo  Ok ) ELSE ( mkdir entrees_csv && echo Dossier entrees_csv créé. )
 start env-python\Scripts\activate.bat
