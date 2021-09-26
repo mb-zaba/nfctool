88ac7cf5788cbbc1b70e8df4c79580d7732a125c
mb-zaba <mathis@bonnin.dev>
2021-09-26 19:56:18 +0200

    Updating the main program

--- nfctool.py
+++ nfctool.py
@@ -5,13 +5,27 @@ import sys
 
 class NFCtool:
     def __init__(self, device):
+        print("Place the NFC Card on the device.")
         self.clf = nfc.ContactlessFrontend(device)
         self.tag = self.clf.connect(rdwr={'on-connect': lambda tag: False})
 
     def read_nfc(self):
-        records = self.tag
+        records = self.tag.ndef
+        print("Readable: " + records.is_readable())
+        print("Writeable: " + records.is_writeable())
         # data = records.text.split('\n')
+        self.clf.close()
         return records
+
+    def write_nfc(self):
+        record = ndef.UriRecord("https://zaba.dev/")
+        print(record.iri)
+        print(record.uri)
+        formatting = self.tag.format()
+        print(formatting)
+        test = b''.join(ndef.message_encoder([record]))
+        self.clf.close()
+        return "Data written."
 
 
 if __name__ == '__main__':
@@ -19,4 +33,4 @@ if __name__ == '__main__':
         raise Exception("Missing argument")
     nfct = NFCtool(sys.argv[1])
     print(nfct.read_nfc())
-    # nfct.clf.close()
+    # print(nfct.write_nfc())
